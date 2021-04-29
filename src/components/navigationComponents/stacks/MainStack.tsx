3e0cf65606a8aa6c65b10656b07a6a1685d908bc
darsshanNair <tysonjasmin@gmail.com>
2021-04-29 16:26:58 +0800

    Implement `New User` screen with form entries

--- src/components/navigationComponents/stacks/MainStack.tsx
+++ src/components/navigationComponents/stacks/MainStack.tsx
@@ -3,10 +3,12 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../../../containers/home/HomeScreen';
 import UserDetailsScreen from '../../../containers/details/UserDetailsScreen';
 import { User } from '../../../models/User';
+import NewUserScreen from '../../../containers/newUser/NewUserScreen';
 
 export type RootStackParamList = {
   Home: undefined;
   UserDetails: { user: User };
+  NewUser: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -23,6 +25,11 @@ export const MainStack = () => {
         name="UserDetails"
         component={UserDetailsScreen}
         options={{ headerTitle: 'User Details' }}
+      />
+      <Stack.Screen
+        name="NewUser"
+        component={NewUserScreen}
+        options={{ headerTitle: 'New User' }}
       />
     </Stack.Navigator>
   );
